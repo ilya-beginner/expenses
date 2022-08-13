57ec5eb6a939b3d8ae7f5750ac6e64de49d77810
ilya-beginner <ilya.beginner@gmail.com>
2022-08-13 18:46:20 +0300

    Initial commit

---
+++ Converter.cs
@@ -0,0 +1,40 @@
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SystemTextJsonSamples
+{
+    public class DateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
+    {
+        public override DateTimeOffset Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) =>
+                DateTimeOffset.ParseExact(reader.GetString()!,
+                    "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTimeOffset dateTimeValue,
+            JsonSerializerOptions options) =>
+                writer.WriteStringValue(dateTimeValue.ToString(
+                    "MM/dd/yyyy", CultureInfo.InvariantCulture));
+    }
+
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        public override DateOnly Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) =>
+                DateOnly.ParseExact(reader.GetString()!,
+                    "yyyy-mm-dd", CultureInfo.InvariantCulture);
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateOnly dateValue,
+            JsonSerializerOptions options) =>
+                writer.WriteStringValue(dateValue.ToString(
+                    "yyyy-MM-dd", CultureInfo.InvariantCulture));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "server=database-expenses.cjdnjnrorbm5.eu-west-1.rds.amazonaws.com;user=admin;password=fanxio468;database=expenses";
+var serverVersion = new MariaDbServerVersion(new Version(10, 6, 8));
+
+builder.Services.AddDbContext<ExpenseDb>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:8080")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                      });
+});
+var app = builder.Build();
+
+app.MapPost("/expense", async (Expense expense, ExpenseDb db) =>
+{
+    db.expenses.Add(expense);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/expense/{expense.id}", expense);
+}).RequireCors(MyAllowSpecificOrigins);
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.Run();
+
+class Expense
+{
+    public int id { get; set; }
+    [JsonConverter(typeof(SystemTextJsonSamples.DateOnlyJsonConverter))]
+    public DateOnly date { get; set; }
+    public float sum { get; set; }
+    public string? tag { get; set; }
+    public string? notes { get; set; }
+}
+
+class ExpenseDb : DbContext
+{
+    public ExpenseDb(DbContextOptions<ExpenseDb> options)
+        : base(options) { }
+
+    public DbSet<Expense> expenses => Set<Expense>();
+}
