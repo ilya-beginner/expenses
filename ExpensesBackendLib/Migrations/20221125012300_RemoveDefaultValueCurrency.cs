6b7ac025d4689719024ad68a68be783e109df1b2
ilya-beginner <ilya.beginner@gmail.com>
2022-11-25 01:28:51 +0300

    db migration: remove default value from "Currency"

---
+++ ExpensesBackendLib/Migrations/20221125012300_RemoveDefaultValueCurrency.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expenses.Migrations
+{
+    public partial class RemoveDefaultValueCurrency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                table: "Expenses",
+                type: "int",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
